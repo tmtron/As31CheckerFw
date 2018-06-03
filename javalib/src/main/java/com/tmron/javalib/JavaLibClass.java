65fd7323ac87a82c0d7343acf0b7be2bfd918419
TMTron <martin.trummer@tmtron.com>
2018-06-03 09:01:56 +0200

    updated checker-framework to 2.5.2 * now we use only one project wide variable for the checker-fw version * also updated some other dependencies * added a failing function to JavaLibClass that can be activated to see if the nullness checker is run correctly

--- javalib/src/main/java/com/tmron/javalib/JavaLibClass.java
+++ javalib/src/main/java/com/tmron/javalib/JavaLibClass.java
@@ -4,8 +4,20 @@ import javax.annotation.Nullable;
 
 public class JavaLibClass {
 
+    private static @Nullable Integer getLengthThresholdNullable() {
+        return null;
+    }
+
+    private static  Integer getLengthThreshold() {
+        return 2;
+    }
+
     public static @Nullable String toNullable(String input) {
-        if (input.length() < 2) {
+        /* when the nullable version is activated, the nullness-checker will report:
+         *  error: [unboxing.of.nullable] unboxing a possibly-null reference getLengthThresholdNullable()
+         */
+        //if (input.length() < getLengthThresholdNullable()) {
+        if (input.length() < getLengthThreshold()) {
             return null;
         } else {
             return input;
